67e295ce7c4df912ed8bba25e27132548ddf5f4f
xiaoxiae <tomas@slama.dev>
2023-09-06 19:25:09 +0200

    Improve elo calculation
    It's not as stupid now?

--- elo.py
+++ elo.py
@@ -1,7 +1,8 @@
 import argparse
 import json
 import os
-from random import choice
+import subprocess
+from random import shuffle
 
 base_agents = [
     "coin_collector_agent",
@@ -19,14 +20,13 @@ def play_games(games_to_play: dict):
     if os.path.exists(GAMES_FILE):
         os.remove(GAMES_FILE)
 
-    while games_to_play:
-        agents = choice(list(games_to_play))
+    for (a1, a2), games in games_to_play.items():
+        print(f"Playing {games} games between {a1} and {a2}...")
 
-        games_to_play[agents] -= 1
-        if games_to_play[agents] == 0:
-            del games_to_play[agents]
-
-        os.system(f"python3 main.py play --agents {agents[0]} {agents[1]} --n-rounds 1 --no-gui")
+        subprocess.Popen(
+            ["python3", "main.py", "play", "--agents", a1, a2, "--n-rounds", str(games), "--no-gui"],
+            stdout=subprocess.DEVNULL
+        ).communicate()
 
 
 def load_results():
@@ -37,15 +37,29 @@ def load_results():
     return results
 
 
+def print_stats():
+    stats = json.load(open(STATS_FILE))
+
+    print("Base agents:")
+    print(json.dumps(stats['base'], indent=4))
+
+    if 'other' in stats:
+        print()
+        print("Other agents:")
+        print(json.dumps(stats['other'], indent=4))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-n",
-                        help="Number of games to play. Defaults to 1000.", action='store_const', default=1000)
+                        help="Number of games to play. Defaults to 100.", action='store_const', default=100)
     parser.add_argument("-k",
                         help="The k-factor for calculating the elo. Defaults to 10.", action='store_const', default=10)
+    parser.add_argument("-b", "--base-elo",
+                        help="Base elo for calculating stuff. Defaults to 1000.", action='store_const', default=1000)
 
-    subparsers = parser.add_subparsers(dest="mode")
+    subparsers = parser.add_subparsers(dest="mode", required=True)
 
     agent_subparser = subparsers.add_parser("agent",
                                             help='Benchmarks the agent against the currently added agents, adding it to the system.')
@@ -55,10 +69,20 @@ if __name__ == "__main__":
     base_subparser = subparsers.add_parser("base",
                                            help="Generates the elos for the base agents.")
 
-    base_subparser.add_argument("--recalculate", help="Recalculate the elos only, don't play the games.",
+    base_subparser.add_argument("--recalculate", help="Recalculate the elos only, don't play the games."
+                                                      "Useful for changing the k-factor.",
                                 action='store_true')
 
+    stats_subparser = subparsers.add_parser("stats",
+                                            help="Prints statistics about the current elos of agents.")
+
     arguments = parser.parse_args()
+
+    if arguments.mode == 'stats':
+        print_stats()
+        quit()
+
+    # first, determine what games we need to play
     games_to_play = {}
 
     if arguments.mode == 'base':
@@ -73,21 +97,36 @@ if __name__ == "__main__":
         for i in range(len(base_agents)):
             games_to_play[(arguments.agent, base_agents[i])] = arguments.n
 
+    # then actually play them (or just recalculate if --recalculate is specified)
     if arguments.mode == 'agent' or (arguments.mode == 'base' and not arguments.recalculate):
         play_games(games_to_play)
         results = load_results()
+
+        # we have to shuffle the results because the order of the games matters quite a bit for calculating elo
+        # this way it somewhat randomizes it, and while it isn't ideal, it's much better than not sorting
+        shuffle(results)
     else:
         results = json.load(open(STATS_FILE))["base_games"]
 
+    # then we create the stats dictionary for base, or load it for agent
     if arguments.mode == 'base':
         stats = {
-            "base": {agent: 1000 for agent in base_agents},
+            "base": {agent: arguments.base_elo for agent in base_agents},
             "base_games": results
         }
     elif arguments.mode == 'agent':
         stats = json.load(open(STATS_FILE))
-        benchmark_agent_elo = 1000
 
+        if "other" not in stats:
+            stats["other"] = {}
+
+        if "other_games" not in stats:
+            stats["other_games"] = {}
+
+        stats["other"][arguments.agent] = arguments.base_elo
+        stats["other_games"][arguments.agent] = results
+
+    # the actual elo calculation
     for a1, result, a2 in results:
         a = stats["base"][a1] if a1 in stats["base"] else stats["other"][a1]
         b = stats["base"][a2] if a2 in stats["base"] else stats["other"][a2]
@@ -101,17 +140,8 @@ if __name__ == "__main__":
             stats["base"][a2] -= difference
 
         elif arguments.mode == 'agent':
-            benchmark_agent_elo += difference
-
-    if arguments.mode == 'agent':
-        if "other" not in stats:
-            stats["other"] = {}
-
-        stats["other"][arguments.agent] = benchmark_agent_elo
-
-        if "other_games" not in stats:
-            stats["other_games"] = {}
-
-        stats["other_games"][arguments.agent] = results
+            stats["other"][a1] += difference
 
     json.dump(stats, open(STATS_FILE, 'w'))
+
+    print_stats()
