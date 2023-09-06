fb12728e1a687d2a9d0c8fbfb3e6e1b9512f25bd
BehroozMontazeran <120589440+BehroozMontazeran@users.noreply.github.com>
2023-09-06 19:41:12 +0200

    Update train.py
    make it to work with different users that we have

--- train.py
+++ train.py
@@ -2,28 +2,40 @@ import argparse
 from pathlib import Path
 import os
 
-TASKS = {
-    "1": [
-        "--no-gui --agents user_agent --scenario coin-heaven --n-rounds 100",
-    ],
-    "2": [
-        "--no-gui --agents user_agent --scenario coin-heaven --n-rounds 100",
-        "--no-gui --agents user_agent rule_based_agent --scenario empty --n-rounds 1000",
-        "--no-gui --agents user_agent --scenario classic --n-rounds 1000",
-        "--no-gui --agents user_agent rule_based_agent --scenario classic --n-rounds 10000",
-    ]
-}
+
+# TASKS = {
+#     "1": [
+#         "--no-gui --agents distance_agent --scenario coin-heaven --n-rounds 100",
+#     ],
+#     "2": [
+#         "--no-gui --agents distance_agent --scenario coin-heaven --n-rounds 50",
+#         "--no-gui --agents distance_agent peaceful_agent --scenario empty --n-rounds 1000",
+#         "--no-gui --agents distance_agent peaceful_agent --scenario classic --n-rounds 10000",
+#     ]
+# }
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("task", help="The task to train.")
+    parser.add_argument("training_agent", help="The agent to train.")
 
     arguments = parser.parse_args()
 
-    for file in Path("agent_code/user_agent").iterdir():
+    tasks = {
+    "1": [
+        f"--no-gui --agents {arguments.training_agent} --scenario coin-heaven --n-rounds 100",
+    ],
+    "2": [
+        f"--no-gui --agents {arguments.training_agent} --scenario coin-heaven --n-rounds 100",
+        f"--no-gui --agents {arguments.training_agent} peaceful_agent --scenario empty --n-rounds 1000",
+        #f"--no-gui --agents {arguments.training_agent} peaceful_agent --scenario classic --n-rounds 10000",
+    ]
+}
+
+    for file in Path(f"agent_code/{arguments.training_agent}").iterdir():
         if file.suffix == ".pt":
             file.unlink()
 
-    for command in TASKS[arguments.task]:
+    for command in tasks[arguments.task]:
         os.system(f"python main.py play --train 1 {command}")
